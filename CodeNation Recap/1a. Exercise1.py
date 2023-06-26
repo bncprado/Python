4d8cb96c1e102af34d1c21957825ac9c83dcd1c6
bncprado <bncprado@gmail.com>
2023-06-26 10:25:05 +0100

    2 new exercises: a and b

---
+++ CodeNation Recap/1a. Exercise1.py
@@ -0,0 +1,17 @@
+"""
+Demonstrate your understanding of the arithmetic operators.
+
+Create a program which accepts two inputs from a user (num1 and num2), use these inputs with each operator.
+
+Print the equation and the output.
+
+"""
+
+num1 = int(input('Type a the first number: '))
+num2 = int(input('Now, type the second number: '))
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+print(num1**num2)
+print(num1%num2)
---
+++ CodeNation Recap/1b. Exercise2.py
@@ -0,0 +1,27 @@
+"""
+Print the follow equations and their answers to the terminal:
+
+4 x 5+ 10 / 5+ 3 
+2**2 + 6 / 3 
+
+Add brackets into the following equations so that each sum is correct – print them to the terminal
+
+4 x 5+ 10 / 5+ 3 = 15 
+2**2 + 6 / 3 = 6
+"""
+
+print("\n1. Equation 1: 4 x 5+ 10 / 5+ 3")
+print(f"Equation 1 Result: {4 * 5+ 10 / 5+ 3}")
+
+
+print("\n2. Equation 2: 2**2 + 6 / 3 ")
+print(f"Equation 2 Result: {2**2 + 6 / 3 }")
+
+
+print("\n3. Add brackets in the following equation to get the result \"15\":\n4 x 5+ 10 / 5+ 3")
+print("ANSWER: 4*(5+ 10)/5+3")
+print(f"Equation 3 Result:{4*(5+ 10)/5+3}")
+
+print("\n4. Add brackets in the following equation to get the result \"16\":\n2**2 + 6 / 3")
+print("ANSWER: 2**(2 + 6 / 3)")
+print(f"Equation 4 Result:{2**(2 + 6 / 3)}\n")
