88852317c8e3d06fec2ecdc6c21b8e490cc72d8d
bncprado <bncprado@gmail.com>
2023-06-26 14:32:49 +0100

    2a updated using while True loop

--- CodeNation Recap/2. If Else/If Else Exercises/2a.py
+++ CodeNation Recap/2. If Else/If Else Exercises/2a.py
@@ -1,10 +1,17 @@
-password = input ("Please type your password: ")
 
-if len(password) < 8:
-    print ("Password too short, please create a new one")
-else:
-    print (f"Your password is \"{password}\"")
 
+# if len(password) < 8:
+#     print ("Password too short, please create a new one")
+# else:
+#     print (f"Your password is \"{password}\"")
+
+while True:
+    password = input ("Please type your password: ")
+    if len(password) < 8:
+      print ("Password too short, please create a new one")
+    else:
+      print (f"Your password is \"{password}\"")
+      break
 
     
-    
+  
