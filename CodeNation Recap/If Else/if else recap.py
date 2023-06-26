f48a40ac60485512d7d3c8a61b35e46b3d7cf3d4
bncprado <bncprado@gmail.com>
2023-06-26 11:43:54 +0100

    progressing with if else

---
+++ CodeNation Recap/If Else/if else recap.py
@@ -0,0 +1,84 @@
+# music = 'classical'
+
+# if music == 'classical':
+#     print("Oh no! It's classical again")
+# elif music == 'no music':
+#     print("Ah, peace an quiet")
+# else:
+#     print ("Turn it up")
+
+# """
+# "=" is an assignment operator. It says the variable "music" is the string “classical”.
+
+# "==" is a comparison operator. It compares the value of our music variable to the string “classical”.
+
+# The program evaluates this comparison to a Boolean. Is it True or False?
+
+# As soon as something is evaluated to True, the matching block of code will run, and the if statement is complete!
+
+# """
+
+# """
+# COMPARISON OPERATORS:
+
+# ==    Equal to 
+# !=    Not equal to 
+# <     Less than 
+# <=    Less than or equal to 
+# >     More than 
+# >=    More than or equal to
+
+# """
+
+# #------------------------------------------------------------------------
+
+# music = "classical"
+
+# if music != "britpop":
+#     print("I want to listen to britpop")
+# else:
+#     print('I love britpop!')
+
+# """
+# You might encounter "!=", that means "not equal".
+# Our first condition here is, if music does not equal “britpop”
+
+# """
+
+# # #------------------------------------------------------------------------
+
+# music = "Oasis"
+
+# if music != "britpop":
+#     print("I want to listen to britpop")
+# elif music == "Oasis":
+#     print("Turn it up!")
+# else:
+#     print("I live britpop!")
+
+# """
+# Even though the elif statement "Oasis" matches with the value of the variable "music", the first condition the code above will run is != (not equal) to the string "britpop".
+
+# An if statement runs the first thing it evaluates to be True even if there is something even more True later! Be aware of the structure of your statement.
+
+# """
+
+# #-----------------------------------------------------------------------------
+
+# # Which one will run?
+# temperature = 80
+
+# if temperature > 99:
+#   print ("It's boiling")
+# elif temperature > 90:
+#   print("It's too hot")
+# elif temperature > 70:
+#   print("It's warm") #this one is the one that will be executed
+# elif temperature > 40:
+#   print("It's just right")
+# else:
+#   print("it's a bit chilly")
+
+# #-----------------------------------------------------------------------------
+
+
