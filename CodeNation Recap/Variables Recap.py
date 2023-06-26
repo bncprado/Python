b7261cba65626ad97170f81dfdeed23d4bbf82da
bncprado <52816323+bncprado@users.noreply.github.com>
2023-06-26 09:23:40 +0100

    Create Variables Recap.py

---
+++ CodeNation Recap/Variables Recap.py
@@ -0,0 +1,23 @@
+# print ('withallthings said'[8].upper())
+
+# #-------------------------------------------------------------------------
+
+# name = 'Bruno'
+# age = 42
+# height = 1.85
+# weight = 95
+# weight_unit = 'KG'
+
+# print(f'My name is {name}. I am {age} years old. My weight is {weight}{weight_unit} and I\'m {height} tall. ')
+
+# #-------------------------------------------------------------------------
+
+# user_name = input('Please, type your name: ')
+
+# print(f"Hello {user_name}")
+
+# "It's important to note that the input will always save a string, unless we specify the data type (later)"
+
+# #-------------------------------------------------------------------------
+
+
