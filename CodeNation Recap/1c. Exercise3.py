aaa1b100059495727c13c78c06e553fdbc6b046f
bncprado <bncprado@gmail.com>
2023-06-26 10:31:00 +0100

    updating 1a

---
+++ CodeNation Recap/1c. Exercise3.py
@@ -0,0 +1,10 @@
+"""
+
+A shop sells apples for 25p per apple.
+
+Create a program which asks a user how many apples they want to buy.
+
+Display the total cost of the apples in both pence, and pounds and pence.
+
+"""
+
