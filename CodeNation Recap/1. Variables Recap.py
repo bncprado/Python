def4547db568376bc55a5d49dd43835a67dcf533
bncprado <bncprado@gmail.com>
2023-06-26 09:53:33 +0100

    variables recap lesson ok

---
+++ CodeNation Recap/1. Variables Recap.py
@@ -0,0 +1,96 @@
+# print ('withallthings said'[8].upper())
+
+# #-------------------------------------------------------------------------
+
+# name = 'Bruno'
+# age = 42
+# height = 1.85
+# weight = 95
+# weight_unit = 'KG'
+
+# print(f'My name is {name}. I am {age} years old. My weight is {weight}{weight_unit} and I\'m {height} tall. ')
+
+# #-------------------------------------------------------------------------
+
+# user_name = input('Please, type your name: ')
+
+# print(f"Hello {user_name}")
+
+# "It's important to note that the input will always save a string, unless we specify the data type (later)"
+
+# #-------------------------------------------------------------------------
+
+# """
+# ARITHMETIC OPERATORS
+# +   Addition
+# -   Subtraction
+# *   Multiplication
+# **  Exponential
+# /   Division
+# %   Modulo (Remainder)
+# """
+# print(1+2)
+# print(10-4)
+# print(4*6)
+# print(3**3)
+# print(15/5)
+# print(15%4)
+
+# balance = 100
+# amount_withdrawn = 50
+
+# balance = balance - amount_withdrawn
+
+# print(balance)
+
+# #------------------------------------------------------------------------
+
+# """
+
+# ASSIGNMENT OPERATORS
+# +=
+# -=
+# *=
+# /=
+
+# """
+
+# balance = 100
+# amount_withdrawn = 50
+# balance = balance - amount_withdrawn
+# print(balance)
+
+# balance +=amount_withdrawn #we're adding 50 back, same as using balance = balance + withdrawn
+
+# print (balance)
+
+# print(9*(8-3)+4+2-6/2)
+
+# #-------------------------------------------------------------------------
+
+# """
+# CASTING
+# When you specify the data type you want
+
+# """
+
+# # num1 = input('Type your number here: ')
+# # num2 = input('Type your number here: ')
+# # print(num1*num2)
+
+# #The code above will return an error cause strings cannot be multiplied (besides the fact they can be "sum"(concatenated), but not in a arithmethical way)
+
+# #To get a specific type from a input, you have to specify it
+
+# num1 = int(input('Type your number here: '))
+# num2 = int(input('Type your number here: '))
+# print(num1*num2)
+
+# """
+# int() is a form of CONSTRUCTOR. We have more constructors like:
+# float() to force a float number and
+# str() to force a string
+# """
+
+# #-------------------------------------------------------------------------
+
