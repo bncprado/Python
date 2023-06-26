25d35b79112d7c1cb4f96d5b27fa60cb599bdbb5
bncprado <bncprado@gmail.com>
2023-06-26 14:28:48 +0100

    some renames and exercise 2a done

---
+++ CodeNation Recap/2. If Else/If Else Exercises/2a.py
@@ -0,0 +1,10 @@
+password = input ("Please type your password: ")
+
+if len(password) < 8:
+    print ("Password too short, please create a new one")
+else:
+    print (f"Your password is \"{password}\"")
+
+
+    
+    
