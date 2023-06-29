fe07775158cc3ac90d3af60dcb562bd4a318bff9
bncprado <bncprado@gmail.com>
2023-06-29 08:42:26 +0100

    exercises 4a and 4b delivered

---
+++ CodeNation Recap/4. Loops/4. Loops Exercises/4a.py
@@ -0,0 +1,10 @@
+""""
+Create a for loop that prints “hello world” 13 times.
+
+Now, convert this into a while loop that does the same job.
+
+"""
+number = 0
+for i in range (13):
+    number += 1
+    print(f"{number}. Hello world")
---
+++ CodeNation Recap/4. Loops/4. Loops Exercises/4b.py
@@ -0,0 +1,13 @@
+""""
+Use a nested for loop to write a program which prints out the times tables from 1 to 12.
+
+Include a linebreak between each times time to make it easier to read.
+
+"""
+
+for i in range (1,13):
+    for x in range (1,13):
+        print(f"{i} x {x} = {i*x}")
+    print("")
+        
+    
