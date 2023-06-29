49927bddec48c217cd243f49d9c884d47127d8e6
bncprado <bncprado@gmail.com>
2023-06-29 11:22:02 +0100

    libraries lesson1 finished

--- CodeNation Recap/5. Libraries/Libraries recap.py
+++ CodeNation Recap/5. Libraries/Libraries recap.py
@@ -66,3 +66,38 @@
 
 # #--------------------------------------------------------------------------------------------------------------------
 
+# """"
+# We can use these libraries to save us a lot of time. Creating a random number game is much easier when I don't have to worry about how to create the randomness.
+# """
+
+# import random
+
+# my_num = 13
+# ran_num = random.randint(1,50)
+
+# while my_num != ran_num:
+#     print(f"The number {ran_num} doesn't match my number {my_num}")
+#     ran_num = random.randint(1,50)
+
+# print(f"The number {ran_num} matches my number {my_num}")
+
+# #--------------------------------------------------------------------------------------------------------------------
+
+# """
+# There are two ways we can import libraries. The whole library and we access all the methods with dot notation or e can just import the modules we need from a library. 
+
+# When we do so, we don't need to use dot notation anymore. We just use the module name.
+
+# Both are going to do the same thing generate a random float. It's generally better to just import what you need.
+
+# """
+
+# from random import uniform
+
+# print(uniform(1,10))
+
+# import random
+
+# print(random.uniform(1,10))
+
+# #--------------------------------------------------------------------------------------------------------------------
