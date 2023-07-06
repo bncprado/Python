ce7c390c76b9dd19f61f13522b266282617af682
bncprado <bncprado@gmail.com>
2023-07-06 16:10:34 +0100

    exercise 6a done

---
+++ CodeNation Recap/6. Functions/Exercises/6a.py
@@ -0,0 +1,11 @@
+def happy_birth_song(name):
+    print(f""" 
+Happy birthday to {name}
+Happy birthday to {name}
+Happy birthday dear {name}
+Happy birthday to you
+    """)
+
+happy_birth_song("Bruno")
+
+    
