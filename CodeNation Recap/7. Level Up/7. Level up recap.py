9b3a7afed0359bfa1fd291b30ecdce494aae746d
bncprado <bncprado@gmail.com>
2023-07-07 10:31:02 +0100

    PROGRESSING WITH LEVEL UP

--- CodeNation Recap/7. Level Up/7. Level up recap.py
+++ CodeNation Recap/7. Level Up/7. Level up recap.py
@@ -70,9 +70,36 @@
 
 # #-------------------------------------------------------------------------
 
-# IF ELSE - The IN operator
+# # IF ELSE - The IN operator
 
+# print("Which colour is the sky?")
+# answer = input("  >  ").lower()
+# if answer == "blue" or answer == "the sky is blue" or answer == "sky is blue": # we could use the OR operator to accept all these answers, but that's very wordy
+#     print("Correct.")
 
+# # We can use the IN operator - a MEMBERSHIP OPERATOR. The IN operator isn't an exact comparison like "==". Instead, it's looking to see if that element is found in the collection it's looking in. This could be a string, a lista, a tuple, etc. As long as "blue" appears somewhere in the answer, it'll be accepted
 
+# print("Which colour is the sky? (You can type any answer that has \"blue\" in it)")
+# answer = input("  >  ").lower()
+# if "blue" in answer:
+#     print("Correct.")
 
+# #-------------------------------------------------------------------------
 
+# IF ELSE - The NOT operator
+
+# The NOT operatir is an inverter. It flips booleans. "True" becomes "False" and the opposite. 
+
+print (not True) #output will be False
+
+shopping_list = ["orange", "apple", "banana"]
+new_item = input("Add a new item to the list: ")
+if new_item not in shopping_list: #without the not operator, a new item would evaluate to False under this code. The "not" operator flips it, so it runs
+    shopping_list.append(new_item)
+print(shopping_list)
+
+# The not operator is useful because it's extremely readable. It makes perfect sense to us to say "if this item is not in the shopping list, add it". It's easier to think of than trying to work out what is True and False ourselves. Let the program do that
+
+#-------------------------------------------------------------------------
+
+# FUNCTIONS - RETURN
