a19d8e9ed6ff917655a53bbc60a665a0e994bf8b
bncprado <bncprado@gmail.com>
2023-07-07 22:55:40 +0100

    dictionaries

--- CodeNation Recap/7. Level Up/7. Level up recap.py
+++ CodeNation Recap/7. Level Up/7. Level up recap.py
@@ -77,7 +77,7 @@
 # if answer == "blue" or answer == "the sky is blue" or answer == "sky is blue": # we could use the OR operator to accept all these answers, but that's very wordy
 #     print("Correct.")
 
-# # We can use the IN operator - a MEMBERSHIP OPERATOR. The IN operator isn't an exact comparison like "==". Instead, it's looking to see if that element is found in the collection it's looking in. This could be a string, a lista, a tuple, etc. As long as "blue" appears somewhere in the answer, it'll be accepted
+# # We can use the IN operator - a MEMBERSHIP OPERATOR. The IN operator isn't an exact comparison like "==". Instead, it's looking to see if that element is found in the collection it's looking in. This could be a string, a list, a tuple, etc. As long as "blue" appears somewhere in the answer, it'll be accepted
 
 # print("Which colour is the sky? (You can type any answer that has \"blue\" in it)")
 # answer = input("  >  ").lower()
@@ -86,20 +86,132 @@
 
 # #-------------------------------------------------------------------------
 
-# IF ELSE - The NOT operator
+# # IF ELSE - The NOT operator
 
-# The NOT operatir is an inverter. It flips booleans. "True" becomes "False" and the opposite. 
+# # The NOT operator is an inverter. It flips booleans. "True" becomes "False" and the opposite. 
 
-print (not True) #output will be False
+# print (not True) #output will be False
 
-shopping_list = ["orange", "apple", "banana"]
-new_item = input("Add a new item to the list: ")
-if new_item not in shopping_list: #without the not operator, a new item would evaluate to False under this code. The "not" operator flips it, so it runs
-    shopping_list.append(new_item)
-print(shopping_list)
+# shopping_list = ["orange", "apple", "banana"]
+# new_item = input("Add a new item to the list: ")
+# if new_item not in shopping_list: #without the not operator, a new item would evaluate to False under this code. The "not" operator flips it, so it runs
+#     shopping_list.append(new_item)
+# print(shopping_list)
 
-# The not operator is useful because it's extremely readable. It makes perfect sense to us to say "if this item is not in the shopping list, add it". It's easier to think of than trying to work out what is True and False ourselves. Let the program do that
+# # The not operator is useful because it's extremely readable. It makes perfect sense to us to say "if this item is not in the shopping list, add it". It's easier to think of than trying to work out what is True and False ourselves. Let the program do that
 
-#-------------------------------------------------------------------------
+# #-------------------------------------------------------------------------
 
-# FUNCTIONS - RETURN
+# # FUNCTIONS - RETURN
+
+# """
+# Functions allow us to write chunks of reusable code. 
+
+# We call the functions when we need to use them.
+
+# Functions typically allow us to hand over dome data, and the function will process it for us. 
+
+# """
+
+# def add_up (num1, num2): #in this example, the function prints the result. But that limits this function. It's now only useful in cases where we want to print out the result
+#     print(num1+num2)
+
+# add_up(2,2) 
+
+# integer_list = [1,2,3,4,5]
+# def add_up_and_append(num1, num2):
+#     integer_list.append(num1+num2)
+
+# add_up_and_append(2,7)
+# print(integer_list)
+# #There are lots of use cases for adding two numbers together. Do we need to write a function for each possible reason? That's not very efficient. 
+
+# # Functions typically allow us to hand over dome data. The function will process it for us - and then the function RETURNS to its caller. 
+# # Part of the functions add_up job is to print num1_num2. 
+# # Call the function add_up inside a print() function. What else appears when you run this?
+
+# print(add_up(2,2)) #If we don't explicitly tell the function what information to return, it returns None
+# #-------------------------------------------------------------------------
+
+# def add_up(num1, num2):
+#     return num1 + num2 #We can use the return keyword to specify what we want the function to give back to the caller
+
+# print(add_up(4,7))
+
+# integer_list = [1,2,3,4,5]
+
+# integer_list.append(add_up(7,22))
+
+# print(integer_list)
+
+# #-------------------------------------------------------------------------
+
+# # TUPLES
+
+# #Same as lists. Difference is that TUPLES are not changeable and the syntax uses brackets instead of square brackets. Like lists, TUPLES are ordered and the items will have the same index position every time
+
+# coffee_order = (
+#     'Alex - Cortado', 
+#     'Ben - Latte', 
+#     "Charlie - Mocha"
+# )
+
+# coffee_order.append("Diane - Cappuccino") # print will say: AttributeError: 'tuple' object has no attribute 'append'
+
+# #TUPLES have only TWO methods
+
+# coffee_order.count("Alex - Cortado") # counts how many times the specified item appears in the collection
+
+# coffee_order.index("Alex - Cortado") # searches the collection for the specified item and returns its index position
+
+# # Because they can't be changed, TUPLES are a much safer kind of collection. Using a TUPLE lowers the chances of a collection getting accidentally changed
+
+# #-------------------------------------------------------------------------
+
+# DICTIONARIES
+
+"""
+In real life, dictionaries hold words and their definitions in pairs. 
+In Python, dictionaries are collections of data. 
+This data is stored in KEY:VALUE pairs
+"""
+
+capital_cities = {
+    "England" : "London", 
+    "Scotland" : "Edinburgh",
+    "Wales" : "Cardiff",
+    "Northern Ireland" : "Belfast",
+    "Ireland" : "Dublin"
+}
+
+"""
+Dictionaries are wrapped in {} curly brackets
+
+Keys and pairs are split by : a colon
+
+Elements are split by , a comma
+
+Whereas lists let us look things up their values using a index, dictionaries let us look things up via the key.
+
+"""
+
+print(capital_cities["Ireland"])
+
+""" 
+Dictionaries can't contain duplicates. Newer values will overwrite old ones. Unlike tuples, dictionaries are changeable. 
+"""
+
+capital_cities = {
+    "England" : "London", 
+    "Scotland" : "Edinburgh",
+    "Wales" : "Cardiff",
+    "Northern Ireland" : "Belfast",
+    "Ireland" : "Dublin",
+    "Wales" : "Swansea"
+}
+
+print(capital_cities)
+
+"""
+
+"""
