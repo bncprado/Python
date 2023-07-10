132aa2eb498688e30281cc80c50e5955f4ae7fff
bncprado <bncprado@gmail.com>
2023-07-10 17:09:15 +0100

    more progress

---
+++ Data Science in Python - DataQuest/Part 1. Python Introduction/Introduction to Python Programming/Python Programming/5. Computer Programs.py
@@ -0,0 +1,45 @@
+"""
+In the previous exercise, we gave the computer three instructions:
+
+print(0.00)
+print(2130805)
+print(4.5)
+
+Collectively, we call all these instructions code. We call each line of instruction a line of code.
+
+When we write code, we program the computer to do something. For this reason, we also call the code we write a computer program (or a program). The program we wrote in the previous exercise had three lines of code, but a program can be as small as one line.
+
+The code we write serves as input to the computer. We call the result of executing the code output.
+
+As we've learned, we can display the output using the print()function. Note, however, that Python executes the code regardless of whether we use print() or not. In the example below, we don't see the output of 0.00 + 6.99 because we did not specify print().
+
+0.00 + 6.99
+print(4.5 - 3.5)
+
+Output
+1.0
+
+Now let's practice writing more code. Refer to the dataset to complete this exercise:
+
+track_name                  price	  currency	  rating_count_tot	  user_rating
+
+Facebook	                  0.0	    USD	        2974676	            3.5
+Instagram	                  0.0	    USD	        2161558	            4.5
+Clash of Clans	            0.0	    USD	        2130805	            4.5
+Fruit Ninja Classic	        1.99	  USD	        698516	            4.5
+Minecraft: Pocket Edition	  6.99	  USD	        522012	            4.5
+
+"""
+
+"""
+Instructions
+
+1. Using the print() function, write a program that sums (adds together) the cost of purchasing the Fruit Ninja Classic and the Minecraft: Pocket Edition apps. Refer to the table for the price of each app.
+
+2. Run the program you wrote by clicking the Run Code button.
+
+3. Check the program you wrote by clicking the Submit Answer button.
+
+"""
+
+print(1.99+6.99)
---
+++ Data Science in Python - DataQuest/Part 1. Python Introduction/Introduction to Python Programming/Python Programming/6. Code Comments.py
@@ -0,0 +1,27 @@
+#to comment a code, use the hash key
+
+"""
+
+or we can wrap a multiline comment inside the three double quotes, like this very one
+
+"""
+
+"""
+Instructions
+
+# Minecraft: Pocket Edition cost, rating count, and user rating
+# print(6.99)
+# print(522012)
+# print(4.5)
+
+Uncomment the three lines of code that specify print() by removing the # symbols
+
+Do not uncomment the first line of code, which includes the general description.
+
+Note that if we do not specify a code comment (#) before (to the left of) the general description, the code will result in a syntax error.
+"""
+
+# Minecraft: Pocket Edition cost, rating count, and user rating
+print(6.99)
+print(522012)
+print(4.5)
