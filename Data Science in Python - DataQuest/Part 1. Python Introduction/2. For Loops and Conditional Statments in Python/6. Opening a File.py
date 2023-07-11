3e245b89f830d37ecfc5a6019f1fb323f41dc975
bncprado <bncprado@gmail.com>
2023-07-11 16:35:09 +0100

    6 done

---
+++ Data Science in Python - DataQuest/Part 1. Python Introduction/2. For Loops and Conditional Statments in Python/6. Opening a File.py
@@ -0,0 +1,13 @@
+"""This lesson required csv files that weren't downloadable. So all the problems were solved inside the coding area in the lesson on the website"""
+
+#Code 
+
+from csv import reader
+
+opened_file = open("AppleStore.csv")
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+print(len(apps_data))
+print(apps_data[0])
+print(apps_data[1:3])
