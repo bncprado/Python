6e7edf066fd6f7199c54a912451e0825873d86e7
bncprado <bncprado@gmail.com>
2023-07-13 09:59:07 +0100

    notes about lists

---
+++ Data Science in Python - DataQuest/LISTS notes.py
@@ -0,0 +1,8 @@
+list_a = [5,6,7,8,9]
+list_b = ["e", "f", "g", "h", "i"]
+list_cd = [[1,2,3,4,5],["a", "b", "c", "d", "e"]]
+
+print(list_a[0])#prints first item of the list
+print(list_b[0])#prints first item of the list
+print(list_cd[1])#prints the second item (that is a list) of the list
+print(list_cd[1][0])#prints first item of the second list
