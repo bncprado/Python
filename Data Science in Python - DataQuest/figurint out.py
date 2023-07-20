352dbb79e9751f43001b2eab5f455e16db430526
bncprado <bncprado@gmail.com>
2023-07-20 08:21:34 +0100

    adding key value to empty dict

--- Data Science in Python - DataQuest/figurint out.py
+++ Data Science in Python - DataQuest/figurint out.py
@@ -1,23 +1,21 @@
-"""how to multiply values using the key of a dictionary"""
-
+"""adding new key and values to an empty dict and updating the values after creating it""" 
 shop_list = {}
 
 
-def question():
-    while True:
-        which_item = input("Which item? ").capitalize()
-        how_many = int(input("How many? "))
-        if which_item in shop_list:
-            shop_list[which_item] += how_many
-        elif which_item not in shop_list:
-            shop_list[which_item.capitalize()] = how_many
-        print(shop_list)
-        x = input("Do you want to continue? Type \"Y\" for yes or anything else to quit: ").capitalize()
-        if x == "Y":
-            continue
-        else:
-            break
+
+while True:
+    which_item = input("Which item? ").capitalize()
+    how_many = int(input("How many? "))
+    if which_item in shop_list:
+        shop_list[which_item] += how_many
+    elif which_item not in shop_list:
+        shop_list[which_item.capitalize()] = how_many
+    print(shop_list)
+    x = input("Do you want to continue? Type \"Y\" for yes or anything else to quit: ").capitalize()
+    if x == "Y":
+        continue
+    else:
+        break
     
-question()
 
 print(shop_list)
