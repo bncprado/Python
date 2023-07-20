d1c49342ff5b3d1099f5597cb97c82063a91edf5
bncprado <bncprado@gmail.com>
2023-07-20 10:15:17 +0100

    I can't believe I finished this project on my own

--- Data Science in Python - DataQuest/figurint out.py
+++ Data Science in Python - DataQuest/figurint out.py
@@ -1,3 +1,5 @@
+import datetime
+
 shop_list = {}
 
 shop_inventory = {
@@ -8,14 +10,43 @@ shop_inventory = {
     'Bread': 1.99,
 }
 
+current_time = datetime.datetime.now().time().hour
+
+name = input("Please, tell us your name: ")
+def greetings():
+    if current_time >= 0 and current_time < 12:
+        print (f"\nGood morning {name}. Welcome to Bruno Groceries.\n")
+    elif current_time >= 12 and current_time < 18:
+        print (f"\nGood afternoon {name}. Welcome to Bruno Groceries.\n")
+    else:
+        print (f"\nGood evening {name}. Welcome to Bruno Groceries.\n")
+
+def items_list():
+    print("\nHere are the items we have available today:\n")
+    print("ITEMS    \tPRICE\n")
+    for key, value in shop_inventory.items():
+        print(f"{key}     \t£{value:.2f}")
+    print("")
+
+greetings()
+
+items_list()
+
+
 while True:
     while True:
-        which_item = input("\nWhich item? ").capitalize()
+        which_item = input("\nPlease type the item you want to buy: ").capitalize()
         if which_item not in shop_inventory:
-            print("Please, choose something that is in our inventory")
+            print(f"\nSorry {name}, we don't have {which_item} today. Please, try something from our inventory.")
+            items_list()
         else:
             break
-    how_many = int(input("\nHow many? "))
+    while True:
+        try: 
+            how_many = int(input(f"\nHow many {which_item}s would you like to buy {name}? "))
+            break
+        except:
+            print(f"I'm sorry {name} but you have to type a number: ")
     if which_item in shop_list:
         shop_list[which_item] += how_many
     elif which_item not in shop_list:
@@ -29,11 +60,24 @@ while True:
     print(f"\nOVERALL TOTAL: £{overall_total:.2f}")
     print("")
 
-    x = input("Do you want to continue? Type \"Y\" for yes or anything else to quit: ").capitalize()
+    x = input(f"Do you want to keep shopping {name}? Type \"Y\" to keep shopping or anything else for payment").capitalize()
     if x == "Y":
+        print("")
+        items_list()
         continue
     else:
         break
-    
 
-print(shop_list)
+print(f"""
+THE TOTAL FOR YOUR BASKET IS £{overall_total:.2f}.
+
+Thank you for shopping with us, {name}.""")
+
+if current_time >= 0 and current_time < 12:
+        print ("\nHave a great day.\n")
+elif current_time >= 12 and current_time < 18:
+        print ("\nEnjoy your afternoon.\n")
+else:
+    print ("\nHave a good evening\n")
+
+
