3366ccb6648f34b2a28460b64255990bd12a53d5
bncprado <bncprado@gmail.com>
2023-07-20 09:33:45 +0100

    I can get price from inventory and multiply by qty

--- Data Science in Python - DataQuest/figurint out.py
+++ Data Science in Python - DataQuest/figurint out.py
@@ -1,16 +1,29 @@
-"""adding new key and values to an empty dict and updating the values after creating it""" 
 shop_list = {}
 
-
+shop_inventory = {
+    'Apple': 0.99,
+    'Banana': 0.5,
+    'Orange': 0.75,
+    'Milk': 2.49,
+    'Bread': 1.99,
+}
 
 while True:
-    which_item = input("Which item? ").capitalize()
-    how_many = int(input("How many? "))
+    which_item = input("\nWhich item? ").capitalize()
+    how_many = int(input("\nHow many? "))
     if which_item in shop_list:
         shop_list[which_item] += how_many
     elif which_item not in shop_list:
-        shop_list[which_item.capitalize()] = how_many
-    print(shop_list)
+        shop_list[which_item.capitalize()] = how_many  
+    print("")
+    print("ITEMS\t\tQTY\t\tTOTAL PRICE\n")
+    overall_total = 0
+    for key, value in shop_list.items():
+        print(f"{key}\t\t\t{value}\t\t\t£{shop_inventory[key]*value:.2f}")
+        overall_total += shop_inventory[key]*value
+    print(f"\nOVERALL TOTAL: {overall_total:.2f}")
+    print("")
+
     x = input("Do you want to continue? Type \"Y\" for yes or anything else to quit: ").capitalize()
     if x == "Y":
         continue
