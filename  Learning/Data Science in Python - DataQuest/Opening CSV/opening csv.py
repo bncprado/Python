625788d6d568106a53d1c5610769a81e8bfeb4d6
bncprado <bncprado@gmail.com>
2023-10-24 15:39:39 +0100

    trying to open a csv

---
+++  Learning/Data Science in Python - DataQuest/Opening CSV/opening csv.py
@@ -0,0 +1,7 @@
+opened_file = open("D:\Coding\Python\ Learning\Data Science in Python - DataQuest\Opening CSV\AppleStore.csv")
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+print(apps_data[:5])
+
