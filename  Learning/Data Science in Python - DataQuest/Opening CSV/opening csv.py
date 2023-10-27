73d55c26f05de1a8188e11af0b9243985cb2b4e8
bncprado <bncprado@gmail.com>
2023-10-27 11:40:11 +0100

    deleting unused files

---  Learning/Data Science in Python - DataQuest/Opening CSV/opening csv.py
+++  Learning/Data Science in Python - DataQuest/Opening CSV/opening csv.py
@@ -1,8 +1,7 @@
-opened_file = open("D:\Coding\Python\ Learning\Data Science in Python - DataQuest\Opening CSV\AppleStore.csv")
-from csv import reader
-read_file = reader(opened_file)
-apps_data = list(read_file)
+opened_file = open("AppleStore.csv")
+# from csv import reader
+# read_file = reader(opened_file)
+# apps_data = list(read_file)
 
-print(apps_data[:5])
-
-sds = "test", 
+# print(apps_data)
+print(opened_file)
