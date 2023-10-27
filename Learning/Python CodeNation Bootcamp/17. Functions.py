0b864448804f61fa0f40ba018d7e9ebcb8e160f6
bncprado <bncprado@gmail.com>
2023-10-27 11:50:01 +0100

    adjusting names

---
+++ Learning/Python CodeNation Bootcamp/17. Functions.py
@@ -0,0 +1,14 @@
+def whatsyourname (username):
+    print (f"\nHappy birthday to {username}\nHappy birthday to {username}\nHappy birthday dear {username}\nHappy birthday to {username} \n")
+
+whatsyourname(input("\nWhat's your name? "))
+
+"""This is a simple and efficient submission with all the necessary pieces. 
+You've defined your function, giving it the parameter to work with in place of knowing the person's name at the point.
+
+Using input for your arguement is great - and more realistic. There wouldn't be many situations where you hard code a name in!
+
+Remember to use snake_case when possible. 
+Though it doesn't impact the functionality of your code, whatsyourname to whats_your_name is a big difference for readability!
+
+Well done!"""
