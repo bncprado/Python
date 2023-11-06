e90a034a9d472cb0383315e032a674a7a2bf5762
bncprado <bncprado@gmail.com>
2023-11-06 14:51:01 +0000

    notes

--- Learning/DataQuest.io - Data Science in Python/Opening CSV/opening csv.py
+++ Learning/DataQuest.io - Data Science in Python/Opening CSV/opening csv.py
@@ -1,7 +1,7 @@
-opened_file = open("AppleStore.csv")
-# from csv import reader
-# read_file = reader(opened_file)
-# apps_data = list(read_file)
+from csv import reader
+opened_file = open("D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\AppleStore.csv")
+read_file = reader(opened_file)
+apps_data = list(read_file)
 
-# print(apps_data)
-print(opened_file)
+print(apps_data)
+
---
+++ practice.py
@@ -0,0 +1,83 @@
+# years = [1999, 2999]
+# words = ['test', 'bruschetta']
+# cities = ['london', 'paris']
+# list = [[1999, 2999], ['test', 'bruschetta'], ['london', 'paris']]
+# for row in list:
+#   print (row[1])
+
+#########################################
+
+# content_ratings = {
+#     "PG-13": 10,
+#     "R": 5,
+#     "G": 3
+# }
+
+# # c_rating = "PG-13"
+# # content_ratings["PG-13"] = content_ratings["PG-13"]+1
+
+# # print(content_ratings)
+
+# # apple = 13
+# # apple = apple + 1
+# # apple +=1
+# # print (apple)
+
+# content_ratings["B"]=2
+# print(content_ratings)
+
+############################################## ADDING AND REMOVING ITEMS TO DICTIONARIES 
+
+# dict = {}
+# dict["novo"]= "novo string"
+# print(dict)
+# dict.clear()
+# print(dict)
+# dict["novo"]= "novo string"
+# dict["velho"]= "velho"
+# dict.pop("novo")
+# print(dict)
+
+
+############################################### ASSIGMENT OPERATORS TEST
+
+# number = 1
+# number += 2
+# print (number)
+
+############################################### 
+
+# number1 = 1
+# number3 = 3
+
+# if 1 < 2 >= 4:
+#   print('opa')
+# else:
+#   print('n')
+
+############################################### FUNCTIONS
+
+# def soma(number, number2):
+#   lista = [number,number2, number]
+#   return print(sum(lista))
+
+# soma(1,2)
+  
+
+############################################## MORE FUNCTIONS
+
+# def onenumber(number):
+#   return number
+
+# def twonumbers(number1, number2):
+#   return number1, number2
+
+# print(type(onenumber(1))) #output: int
+# print(type(twonumbers(1,2))) #output: tuple
+
+############################################## TUPLES
+umatuple = (1,2)
+umatuple_tambem = 1,2
+
+print(type(umatuple)) #output: <class 'tuple'>
+print(type(umatuple_tambem)) #output: <class 'tuple'>
