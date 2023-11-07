43d5f209ca746449798f49bd9f1bea9d2d0b62eb
bncprado <bncprado@gmail.com>
2023-11-07 10:15:06 +0000

    opening csv is working now in vsc

--- Learning/DataQuest.io - Data Science in Python/Opening CSV/opening csv.py
+++ Learning/DataQuest.io - Data Science in Python/Opening CSV/opening csv.py
@@ -1,7 +1,7 @@
 from csv import reader
-opened_file = open("D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\AppleStore.csv")
+opened_file = open("D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\AppleStore.csv", encoding='utf8')
 read_file = reader(opened_file)
 apps_data = list(read_file)
 
-print(apps_data)
+print(apps_data[:1])
 
