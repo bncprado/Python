763a9ef7ce48e8bff4f77fb4e4a66e7300aa6701
bncprado <bncprado@gmail.com>
2023-12-04 14:35:22 +0000

    numpy boolean

--- practice.py
+++ practice.py
@@ -264,25 +264,41 @@
 
 ################################################## introducing numpy
 
-from csv import reader
-opened_file_ios = open('D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\AppleStore.csv', encoding='utf8')
-opened_file_android = open('D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\googleplaystore.csv', encoding='utf8')
-read_file_ios = reader(opened_file_ios)
-read_file_android = reader(opened_file_android)
-ios_data = list(read_file_ios)
-android_data = list(read_file_android)
+# from csv import reader
+# opened_file_ios = open('D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\AppleStore.csv', encoding='utf8')
+# opened_file_android = open('D:\Coding\Python\Learning\DataQuest.io - Data Science in Python\Opening CSV\googleplaystore.csv', encoding='utf8')
+# read_file_ios = reader(opened_file_ios)
+# read_file_android = reader(opened_file_android)
+# ios_data = list(read_file_ios)
+# android_data = list(read_file_android)
 
-import numpy
+# import numpy
 
-ios_new = numpy.array(ios_data)
+# ios_new = numpy.array(ios_data)
 
-print(ios_new[0]) #prints the entire row 0
-print(ios_new[0,0]) #prints the row 0, column 0
-print(ios_new[:,0]) #prints the entire column 0
-print(ios_new[:,:]) #prints all rows and columns
-print(ios_new[:1,:10]) #prints row 0, columns from 0 to 9
-print(ios_new[:3,:3]) #prints row 0 to 2, columns from 0 to 2
-print(ios_new[:,[1,4,5]]) #prints all rows, columns from 1,4 and 5  
-print(ios_new[[0,4,5],:]) #prints rows 0,4,5 and all columns
-print(ios_new[[0,1,2],[0,1,2]]) #prints row 0, column 0; row 1, column1; row2, column2 
+# print(ios_new[0]) #prints the entire row 0
+# print(ios_new[0,0]) #prints the row 0, column 0
+# print(ios_new[:,0]) #prints the entire column 0
+# print(ios_new[:,:]) #prints all rows and columns
+# print(ios_new[:1,:10]) #prints row 0, columns from 0 to 9
+# print(ios_new[:3,:3]) #prints row 0 to 2, columns from 0 to 2
+# print(ios_new[:,[1,4,5]]) #prints all rows, columns from 1,4 and 5  
+# print(ios_new[[0,4,5],:]) #prints rows 0,4,5 and all columns
+# print(ios_new[[0,1,2],[0,1,2]]) #prints row 0, column 0; row 1, column1; row2, column2 
   
+
+################################################## numpy boolean vector
+import numpy as np
+
+arr = np.array(['linha 1', 'linha 2', 'linha 3', 'linha 4'])
+
+print(arr)
+
+bool1 = [True, False, True, False]
+
+print(arr[bool1]) 
+
+print(arr[[True, False, True, False]])
+
+
+
